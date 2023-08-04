21ca661daa87cacf00ac7c68bec1804f59f4fa3c
RawChen <2221999792@qq.com>
2023-08-04 23:59:19 +0800

    fix: special symbols escape

--- src/main/java/com/rawchen/feishubot/util/StringUtil.java
+++ src/main/java/com/rawchen/feishubot/util/StringUtil.java
@@ -6,26 +6,26 @@ package com.rawchen.feishubot.util;
  */
 public class StringUtil {
 	public static String replaceSpecialSymbol(String str) {
-		str = str.replaceAll(">", "&#62;");
-		str = str.replaceAll("<", "&#60;");
-		str = str.replaceAll("~", "&sim;");
-		str = str.replaceAll("-", "&sim;");
-		str = str.replaceAll("!", "&sim;");
-		str = str.replaceAll("\\*", "&sim;");
-		str = str.replaceAll("/", "&sim;");
-		str = str.replaceAll("\\\\", "&sim;");
-		str = str.replaceAll("\\[", "&sim;");
-		str = str.replaceAll("]", "&sim;");
-		str = str.replaceAll("\\(", "&sim;");
-		str = str.replaceAll("\\)", "&sim;");
-		str = str.replaceAll("#", "&sim;");
-		str = str.replaceAll(":", "&sim;");
-		str = str.replaceAll("\\+", "&sim;");
-		str = str.replaceAll("\"", "&sim;");
-		str = str.replaceAll("'", "&sim;");
-		str = str.replaceAll("`", "&sim;");
-		str = str.replaceAll("\\$", "&sim;");
-		str = str.replaceAll("_", "&sim;");
+		str = str.replaceAll(">", 		"&#62;");
+		str = str.replaceAll("<", 		"&#60;");
+		str = str.replaceAll("~", 		"&sim;");
+		str = str.replaceAll("-", 		"&#45;");
+		str = str.replaceAll("!", 		"&#33;");
+		str = str.replaceAll("\\*", 	"&#42;");
+		str = str.replaceAll("/", 		"&#47;");
+		str = str.replaceAll("\\\\",	"&#92;");
+		str = str.replaceAll("\\[", 	"&#91;");
+		str = str.replaceAll("]", 		"&#93;");
+		str = str.replaceAll("\\(", 	"&#40;");
+		str = str.replaceAll("\\)", 	"&#41;");
+		str = str.replaceAll("#", 		"&#35;");
+		str = str.replaceAll(":", 		"&#58;");
+		str = str.replaceAll("\\+", 	"&#43;");
+		str = str.replaceAll("\"", 		"&#34;");
+		str = str.replaceAll("'", 		"&#39;");
+		str = str.replaceAll("`", 		"&#96;");
+		str = str.replaceAll("\\$", 	"&#36;");
+		str = str.replaceAll("_", 		"&#95;");
 		return str;
 	}
 }
